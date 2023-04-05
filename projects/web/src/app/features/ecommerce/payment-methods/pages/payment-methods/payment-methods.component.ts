0601bf113b8437e9852962bc5a021a284e440d5f
David Borge <david.borge.olmedo@gmail.com>
2023-04-05 19:21:59 +0200

    Web: Payment Methods Page - No Payment Methods

--- projects/web/src/app/features/ecommerce/payment-methods/pages/payment-methods/payment-methods.component.ts
+++ projects/web/src/app/features/ecommerce/payment-methods/pages/payment-methods/payment-methods.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-payment-methods',
   templateUrl: './payment-methods.component.html',
-  styleUrls: ['./payment-methods.component.scss']
+  styleUrls: ['./payment-methods.component.scss'],
+  host: {
+    class:'app-payment-methods-classes-for-router-outlet'
+  },
 })
 export class PaymentMethodsComponent {
+
+  // TODO:
+  numberOfPaymentMethods :number = 3;
 
 }
