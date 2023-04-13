f6eabc746514707e57993e57e061efce876cd884
David Borge <david.borge.olmedo@gmail.com>
2023-04-13 19:03:40 +0200

    Web: Loading Screen - Corrección al botón
    El botón solo está activo cuando es visible (opacity: 1).

--- projects/web/src/app/features/ecommerce/loading-screen/pages/loading-screen/loading-screen.component.ts
+++ projects/web/src/app/features/ecommerce/loading-screen/pages/loading-screen/loading-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterContentInit } from '@angular/core';
 
 import { Router, ActivatedRoute } from '@angular/router';
 
@@ -11,17 +11,30 @@ import { Router, ActivatedRoute } from '@angular/router';
     class:'app-loading-screen-classes-for-router-outlet'
   },
 })
-export class LoadingScreenComponent {
+export class LoadingScreenComponent implements AfterContentInit {
+
+  // Loading Screen - "Let's begin" button
+  @ViewChild('letsBeginBtnLocalReference', {static: true}) letsBeginBtnLocalReferenceViewChild: ElementRef = {} as ElementRef;
+  letsBeginBtnHTMLInputElement = {} as HTMLInputElement;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,  // Ruta actual para la configuración de router.navigate(). Documentación: https://angular.io/api/router/ActivatedRoute
   ) { }
 
+  ngAfterContentInit(): void {
+
+    // Comprobacion
+    console.log(this.letsBeginBtnHTMLInputElement);
+  }
+
   onClickLetsBeginButton() {
+    this.letsBeginBtnHTMLInputElement = this.letsBeginBtnLocalReferenceViewChild.nativeElement;  // Uso .nativeElement para pasar del ElementRef al elemento tipo HTML (HTMLInputElement), que es el que me interesa
+
+    console.log(this.letsBeginBtnHTMLInputElement);
     
     // Ir a la Home Page
-    this.router.navigate(['/home']); // Ruta absoluta
+    // this.router.navigate(['/home']); // Ruta absoluta
 
   }
 
