16b703811af5b457efecbbf8f6b1bb80c7ce9628
David Borge <david.borge.olmedo@gmail.com>
2026-03-03 17:41:32 +0100

    test: fix unit test

--- projects/web/src/app/core/services/routing/routing.service.spec.ts
+++ projects/web/src/app/core/services/routing/routing.service.spec.ts
@@ -1,6 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 
+import { provideStore } from '@ngrx/store';
+
+import * as fromApp from '@core/store/app.reducer';
+
 import { RoutingService } from './routing.service';
 
 describe('RoutingService', () => {
@@ -8,7 +12,7 @@ describe('RoutingService', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [provideRouter([])],
+            providers: [provideRouter([]), provideStore(fromApp.appReducer)],
         });
         service = TestBed.inject(RoutingService);
     });
